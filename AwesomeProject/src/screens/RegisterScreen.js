8f4eb05e8c9b03a85a52ed0f6f443e303a578384
Abhishek Parmar <abhishekparmar220197@gmail.com>
2022-06-22 22:50:52 +0530

    Screen

---
+++ AwesomeProject/src/screens/RegisterScreen.js
@@ -0,0 +1,13 @@
+import react from 'react';
+import {View ,Text} from 'react-native'
+
+
+const RegisterScreen = () => {
+    return (
+        <View>
+            <Text>Register Screen</Text>
+        </View>
+    )
+}
+
+export default RegisterScreen;
